cc65e27e0b78b1f6b5a5dcf877a9b2867a27c2a3
Jagankoneti <jagankoneti1@gmail.com>
2026-03-04 10:37:17 +0530

    function completed successfully and also added README file

--- Kangaroo_function/Number_Line_Jumps.js
+++ Kangaroo_function/Number_Line_Jumps.js
@@ -14,7 +14,11 @@
 function kangaroo(x1, v1, x2, v2) {
     // Write your code here
     // console.log('hi');
-    return x1;
+    if(v1<=v2) return 'NO';
+
+    if((x1-x2) % (v1-v2) == 0) return 'YES';
+
+    return 'NO';    
 }
 
 console.log(kangaroo(0, 3, 4, 2));
