399769928c2bdcb11262f1ccbcdedc16211db28f
Jagankoneti <jagankoneti1@gmail.com>
2026-03-04 10:16:55 +0530

    kangaroo problem function create

---
+++ Kangaroo_function/Number_Line_Jumps.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*
+ * Complete the 'kangaroo' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+
+function kangaroo(x1, v1, x2, v2) {
+    // Write your code here
+    // console.log('hi');
+    return x1;
+}
+
+console.log(kangaroo(0, 3, 4, 2));
